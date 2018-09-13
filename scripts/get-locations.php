192a897786ed9c21b6684c728eb2129f748e7163
Xicnet DEVs <code@xicnet.com>
2018-09-13 07:57:24 +0000

    Adapt get-locations.php script to export CSV compatible hyperlinked URLs (for Ethercalc)

--- scripts/get-locations.php
+++ scripts/get-locations.php
@@ -3,7 +3,7 @@
 require_once( '../site/wp/wp-load.php' );
 
 // get all posts
-$posts = get_posts( array (  'numberposts' => -1, 
+$posts = get_posts( array (  'numberposts' => -1,
                              'order' => 'ASC',
                              'orderby' => 'title',
                              'post_type' => 'job_listing' )
@@ -14,6 +14,12 @@ foreach ( $posts as $post )
 {
         if($post->_job_location != "") {
 
-                printf("%s, \"%s\",\"%s\"\n", $post->ID, $post->post_title, $post->_job_location);
-        }   
+                $post_url      = get_site_url().'/?p='.$post->ID;
+                $post_edit_url = admin_url( 'post.php?post='.$post->ID.'&action=edit&lang=en' );
+                # for librecalc
+                #printf("%s, \"%s\",\"%s\",\"=HIPERVÍNCULO(\"\"%s\"\";\"\"edit\"\")\"\n", $post->ID, $post->post_title, $post->_job_location, $post_edit_url);
+
+                # for ethercalc
+                printf("%s, \"%s\",\"%s\",\" \"edit\"<%s>\"\n", $post->ID, $post->post_title, $post->_job_location, $post_edit_url);
+        }
 }
